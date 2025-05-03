4d82477c73ea51e0056bb05e48d23b690a0f862a
Rafah Fajri Juwaeni <rafahfajrijuwaeni@gmail.com>
2025-05-03 15:19:19 +0700

    fix SiteMap

---
+++ app/robots.txt/route.js
@@ -0,0 +1,14 @@
+export async function GET() {
+  return new Response(
+    `User-agent: *
+Allow: /
+
+Sitemap: https://smkalaminpamijahan.vercel.app/sitemap.xml
+`,
+    {
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    }
+  );
+}
